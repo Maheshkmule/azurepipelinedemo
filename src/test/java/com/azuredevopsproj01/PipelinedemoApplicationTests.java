3e62b51193cc43a989891291d8a336469a6c59be
Maheshkmule <maheshkishanrao@gmail.com>
2022-06-23 09:27:53 +0530

    Initial commit

---
+++ src/test/java/com/azuredevopsproj01/PipelinedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuredevopsproj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
